387b4d506e6cd39514a36bb1583827b0010272e3
121111333 RIELISSON JOSEFF DE SOUSA COSTA <rielisson.joseff.costa@lcc1-51.lcc.ufcg.edu.br>
2022-09-29 13:54:38 -0300

    Iniciando refatoramento de codigo da criptografia e organizando para conceito de criptografia ponta-a-ponta

--- src/Login/Cripto.java
+++ src/Login/Cripto.java
@@ -5,19 +5,29 @@ import java.util.Random;
 
 public class Cripto {
 	
-	String[] chars1 = {"q", "e", "t", "u", "o", "l", "j", "g", "d", "a", "x", "v", "n"};
-	String[] chars2 = {"w", "r", "y", "i", "p", "k", "h", "f", "s", "z", "c", "b", "m"};
-	HashSet<String> letras1 = new HashSet<>();
-	HashSet<String> letras2 = new HashSet<>();
-	String[] charsTodos = {"w", "r", "y", "i", "p", "k", "h", "f", "s", "z", "c", "b", "m", "q", "e", "t", "u", "o", "l", "j", "g", "d", "a", "x", "v", "n"};
+	String[] grupoSoma7  = {"q", "e", "t", "u", "o", "l"};
+	String[] grupoSoma12 = {"w", "r", "y", "i", "p", "k"};
+	String[] grupoSoma20 = {"j", "g", "d", "a", "x", "v", "n"};
+	String[] grupoSoma9  = {"h", "f", "s", "z", "c", "b", "m"};
+	String[] alfabeto = {"w", "r", "y", "i", "p", "k", "h", "f", "s", "z", "c", "b", "m", "q", "e", "t", "u", "o", "l", "j", "g", "d", "a", "x", "v", "n"};
+	HashSet<String> setSoma7 = new HashSet<>();
+	HashSet<String> setSoma12 = new HashSet<>();
+	HashSet<String> setSoma20 = new HashSet<>();
+	HashSet<String> setSoma9 = new HashSet<>();
 	Random gerador = new Random();
 	
 	public Cripto() {
-		for (String letra : this.chars1) {
-			this.letras1.add(letra);
+		for (String letra : this.grupoSoma7) {
+			this.setSoma7.add(letra);
 		}
-		for (String letra : this.chars2) {
-			this.letras2.add(letra);
+		for (String letra : this.grupoSoma12) {
+			this.setSoma12.add(letra);
+		}
+		for (String letra : this.grupoSoma20) {
+			this.setSoma20.add(letra);
+		}
+		for (String letra : this.grupoSoma9) {
+			this.setSoma9.add(letra);
 		}
 	}
 	
@@ -25,24 +35,17 @@ public class Cripto {
 		String criptografia = "";
 		int soma;
 		
-		for (String letra : this.chars1) {
-			this.letras1.add(letra);
-		}
-		for (String letra : this.chars2) {
-			this.letras2.add(letra);
-		}
-		
 		for (char caractere : senha.toCharArray()) {
 			int indice = gerador.nextInt(26);
-			if (this.letras1.contains(this.charsTodos[indice])) { 
+			if (this.letras1.contains(this.alfabeto[indice])) { 
 				soma = 7;
 			} else {
 				soma = 12;
 			}
 			String hex = String.format("%x", ((int) caractere + soma));
-			criptografia += hex + this.charsTodos[indice];
+			criptografia += hex + this.alfabeto[indice];
 		}
-		
+
 		return criptografia;
 	}
 	
@@ -60,5 +63,31 @@ public class Cripto {
 			sb.append((char) charInt);
 		}
 		return sb.toString();
+	}
+
+	private quebrarDoService(String senha) {
+		String senhaInvertida = '';
+		String retorno;
+		char[] letras = senha.toCharArray;
+		for (int i = letras.length; i > -1 ; i--) {
+			senhaInvertida += letras[i];
+		}
+
+		for (chat letra : senhaInvertida.toCharArray) {
+			String letraAleatoria = this.alfabeto[gerador.nextInt(26)];
+			if (this.setSoma7.contains(letraAleatoria)) { 
+				soma = 7;
+			} else if (this.setSoma12.contains(letraAleatoria)) {
+				soma = 12;
+			} else if (this.setSoma20.contains(letraAleatoria)) {
+				soma = 20;
+			} else {
+				soma = 9;
+			}
+
+			String hexadecimal = String.format("%x", ((int) letra + soma));
+			String retorno += hexadecimal + letraAleatoria;
+		}
+		return retorno;
 	}
 }
