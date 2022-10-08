5762a32f35a8c598067b863c7a5736fe77d373d0
Costa Joseff <rielisson.joseff.costa@ccc.ufcg.edu.br>
2022-10-08 20:55:31 -0300

    Criptografia distribuída para serveice e repository.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
-import Login.Cripto;
+class Main{
 
-public class Main {
+    public static void main (String[] args) {
+    	char[] palavra = {'a', 'n', 'a'};
+    	System.out.println(eh_palindromo(palavra));
+        
+    }
 
-	public static void main(String[] args) {
-		Cripto c = new Cripto();
-		String a = "123456";
-		String b = c.quebrarDoService(c.quebrarDoService(a));
-		String d = c.montarDoRepo(c.montarDoRepo(b));
-		System.out.println(b);
-		System.out.println(d);
+	private static boolean eh_palindromo(char[] palavra) {
+		
+		return null;
 	}
+
+    
+
 }
